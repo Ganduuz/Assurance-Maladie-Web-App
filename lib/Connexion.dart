1ec9ae5fa7226e4d40d8331fed8b428ac9b3cfc4
Gandouz Akrem <gandouzakrem48@gmail.com>
2024-02-27 16:44:25 +0100

    27/02

--- lib/Connexion.dart
+++ lib/Connexion.dart
@@ -102,10 +102,8 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                       ),
-                       Positioned(
-                         left: 500,
-                         top:40 ,
-                         bottom: 100,
+                      Padding(
+                      padding: EdgeInsets.fromLTRB(250.0, 4.0, 5.0, 2),
                           child:MouseRegion(
                            cursor: SystemMouseCursors.click,
                            child: GestureDetector(
@@ -116,68 +114,69 @@ class MyHomePage extends StatelessWidget {
                                  child: const Text(
                                    'Mot de passe oublié ?',
                                    style: TextStyle(
+                                    fontSize: 14,
                                     color: Color.fromARGB(255, 160, 180, 190),
                                     fontFamily:'InriaSans-Light',
                                    ),
                                  ),
                             ),
-                            ),
-                        ),
+                ),
+                ),
                       SizedBox(height: 55.0),
                       ElevatedButton(
-            style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(0),
-            shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30)),
+  style: ElevatedButton.styleFrom(
+    padding: const EdgeInsets.all(0),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(30)),
     ),
   ),
-  
-  child: Container(
-   
-    decoration: BoxDecoration(
-      
-      borderRadius: BorderRadius.circular(15),
-      gradient: const LinearGradient(
-        colors: <Color>[
-          Color.fromARGB(255,26,149,251),
-          Color.fromARGB(255, 116, 196, 242),
-          Color.fromARGB(255, 153, 196, 237),
-        ],
+  onPressed: () {
+    // Gérer la logique de connexion ici
+  },
+  child: InkWell(
+    splashColor: Colors.transparent, // Pour désactiver le splash par défaut
+    hoverColor: const Color.fromARGB(255, 16, 46, 71).withOpacity(0.8), // Couleur de fond au survol
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: const LinearGradient(
+          colors: <Color>[
+            Color.fromARGB(255, 26, 149, 251),
+            Color.fromARGB(255, 116, 196, 242),
+            Color.fromARGB(255, 153, 196, 237),
+          ],
+        ),
       ),
-    ),
-    padding: const EdgeInsets.fromLTRB(70, 10, 70, 10),
-
-                          child: const Text('Se connecter',
-                      style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFFFFFFFF),
-                      fontFamily: 'Istok web',
-                      )
-                     ),
-  ),
-            onPressed: () {
-              // Gérer la logique de connexion ici
-            }
-            
-                      ),
-                      SizedBox(height: 190), // Espace entre le bouton et le texte 
-               Positioned(
-              top: 0, 
-              left: 500, 
-      child: Text(
-        'Votre bien-être est notre priorité !',
+      padding: const EdgeInsets.fromLTRB(70, 10, 70, 10),
+      child: const Text(
+        'Se connecter',
         style: TextStyle(
-          fontSize: 13,
-         color: Color(0xFF5BADE9),
-          fontFamily: 'Karma-Regular', 
+          fontSize: 16,
+          color: Color(0xFFFFFFFF),
+          fontFamily: 'Istok web',
         ),
       ),
     ),
+  ),
+),
+
+                      SizedBox(height: 190), // Espace entre le bouton et le texte
+                      Positioned(
+                        top: 0,
+                        left: 500,
+                        child: Text(
+                          'Votre bien-être est notre priorité !',
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Color(0xFF5BADE9),
+                            fontFamily: 'Karma-Regular',
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
-
               Align(
                 alignment: Alignment.bottomLeft,
                 child: Container(
@@ -188,27 +187,56 @@ class MyHomePage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(17.0),
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.fromLTRB(30.0, 100.0, 80.0, 20.0),
+                    padding: const EdgeInsets.fromLTRB(20.0, 120.0, 5.0, 20.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'BIENVENUE !',
+                          'Bienvenue !',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 30,
-                            fontFamily: 'Aller_Std_Bd',
+                            fontSize: 44,
+                            fontFamily: 'PlayfairDisplay-Regular',
                           ),
                         ),
-                        SizedBox(height: 30),
-                        Text(
-                          'Connectez-vous pour accéder à votre espace personnel et profiter de tous les avantages de notre plateforme d\'assurance médicale dédiée aux employés de Capgemini Engineering Tunisie.',
-                          style: TextStyle(
+                        SizedBox(height: 70),
+                         Positioned(
+                top: 120,
+                left: 15,
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.5,
+                      fontFamily: 'Open Sans Regular',
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Connectez-vous pour accéder à votre espace personnel et profiter de tous les avantages de notre plateforme d\'assurance médicale dédiée aux employés de ',
+                        style: TextStyle(
                             color: Colors.white,
                             fontSize: 16.5,
                             fontFamily: 'Open Sans Regular',
-                          ),
+                      ),
+                      ),
+                      TextSpan(
+                        text: 'Capgemini Engineering Tunisie',
+                        style: TextStyle(
+                          color: Color(0xFF7C78AA),
+                          fontSize: 16.5,
+                          fontFamily: 'Open Sans Regular',
                         ),
+                      ),
+                      TextSpan(
+                        text: '.',
+                        style: TextStyle(
+                            color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
                       ],
                     ),
                   ),
@@ -217,17 +245,17 @@ class MyHomePage extends StatelessWidget {
               Positioned(
                 top: 1,
                 bottom: 610,
-                left: 95.0,
+                left: 70.0,
                 child: Image.asset(
                   'assets/CapgeminiEngineering_82mm.png',
-                  width: 200,
-                  height: 250,
+                  width: 270,
+                  height: 299,
                 ),
               ),
               Positioned(
                 top: 200.0,
                 bottom: 2.0,
-                left: 14.0,
+                left: 15.0,
                 child: SizedBox(
                   width: 350.0, // Largeur de l'image
                   height: 450, // Hauteur de l'image
